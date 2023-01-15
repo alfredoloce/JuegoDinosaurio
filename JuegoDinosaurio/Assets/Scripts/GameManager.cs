da903481005a870f55062ce2ea9370a3614dde5e
Alfredo Lopez <91043285+alfredoloce@users.noreply.github.com>
2023-01-14 18:21:15 -0600

    BaseJuego

---
+++ JuegoDinosaurio/Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class GameManager : MonoBehaviour
+{   
+    [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private float initialScrollSpeed;
+    private int score;
+    private float timer;
+    private float scrollSpeed;
+    public static GameManager Instance {get; private set;}
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateScore();
+        UpdateSpeed();
+    }
+
+    public void ShowGameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+    }
+
+    private void UpdateScore()
+    {
+        int scorePerSecond = 10;
+        
+        timer += Time.deltaTime;
+        score = (int)(timer * scorePerSecond);
+        scoreText.text = string.Format("{0:00000}", score);
+    }
+
+    public float GetScrollSpeed()
+    {
+        return scrollSpeed;
+    }
+
+    private void UpdateSpeed()
+    {   float speedDivider = 10f;
+        scrollSpeed = initialScrollSpeed + timer / speedDivider;
+    }
+}
